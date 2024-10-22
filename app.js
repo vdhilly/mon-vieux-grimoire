d50f2f9a599a5206d610a338a88d4c7fe1a6016e
vdhilly <65048905+vdhilly@users.noreply.github.com>
2024-10-22 11:29:17 +0200

    base app & server

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const userRoute = require("./routes/userRoute")
+const bookRoute = require("./routes/bookRoute")
+
+mongoose.connect(process.env.MONGODB,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use("/api/auth", userRoute);
+
+module.exports = app;
