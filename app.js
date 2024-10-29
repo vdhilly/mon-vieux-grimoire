78bed94e5c7f8f494bf18d1365cf478b32b1a055
vdhilly <65048905+vdhilly@users.noreply.github.com>
2024-10-29 13:51:48 +0100

    little changes

--- app.js
+++ app.js
@@ -2,23 +2,22 @@ const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const userRoute = require("./routes/userRoute")
-const bookRoute = require("./routes/bookRoute")
+const userRoute = require("./routes/userRoute");
+const bookRoute = require("./routes/bookRoute");
 
-mongoose.connect(process.env.MONGODB,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose
+  .connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   next();
 });
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController.js");
 
+router.post("/signup", userController.signup);
+router.post("/login", userController.login);
 
-router.post('/signup', userController.signup);
-router.post('/login', userController.login);
-
-module.exports = router
+module.exports = router;
